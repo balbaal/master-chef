119e5106542c7526806351c14273ce4d807be2b1
Balbaal <iqbal.mi20@gmail.com>
2020-09-19 13:57:34 +0800

    add: get value search from submit form

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("script js run . . ...")
+const formElement = document.querySelector("form");
+const inputFormElement = document.querySelector("input");
+
+let searchValue = "";
+
+formElement.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchValue = inputFormElement.value;
+
+  console.log("searchValue :>> ", searchValue);
+});
